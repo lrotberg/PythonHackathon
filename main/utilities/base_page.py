1c176a42ca14c3971252fe173d489a1272c5f95f
Lior <Exolite@gmail.com>
2021-12-26 13:46:11 +0200

    Reformat project

--- main/utilities/base_page.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-
-class BasePage:
-    global driver
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-    driver.maximize_window()
