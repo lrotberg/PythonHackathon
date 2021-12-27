6759e6f1554e0b4f04e8d300e0c5c26403b3703a
Lior Rotberg <Exolite@gmail.com>
2021-12-27 09:12:23 +0200

    Added pages

--- test/test_testy.py
+++ test/test_testy.py
@@ -1,8 +1,13 @@
+import time
 import pytest
 
-import test
+import test.conftest as conf
+from pages.web_pages.sign_in_page import SignInPage
 
 @pytest.mark.usefixtures("init_web")
 class Test_Testy:
     def test_01(self):
-        print(test.coftest.driver)
+        expected_balance = 168137
+        conf.driver.get("http://localhost:3000/signin")
+        sign_in_page = SignInPage(conf.driver)
+        sign_in_page.login("Katharina_Bernier", "s3cret")
--- utilities/common_ops.py
+++ utilities/common_ops.py
@@ -1,2 +1,5 @@
-class CommonOps:
-    pass
+from xml.etree import ElementTree as ET
+
+def get_data(self, node_name):
+    root = ET.parse("./data.xml").getroot()
+    return root.find(".//" + node_name).text
