850afb2f52da824dd846380f737f9f8e1aad2743
michal <michal3951@gmail.com>
2021-12-27 14:01:05 +0200

    wf_desktop

--- pages/desktop_pages/calculator_page.py
+++ pages/desktop_pages/calculator_page.py
@@ -1,28 +1,24 @@
-from appium import webdriver
 from selenium.webdriver.common.by import By
 
+class DesktopPage:
 
-class Test_Desktop:
+    def __init__(self,driver):
+        self.driver = driver
 
-    def setup_class(self):
-        global driver
-        desired_caps = {}
-        desired_caps["app"] = "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App"
-        desired_caps["platformName"] = "Windows"
-        desired_caps["deviceName"] = "WindowsPC"
-        driver = webdriver.Remote("http://127.0.0.1:4723", desired_caps)
-        driver.implicitly_wait(5)
+    def get_one(self):
+        return self.driver.find_element(By.NAME,"One").click()
 
-    def test_01(self):
-        expected_result = "6"
-        driver.find_element(By.NAME,"One").click()
-        driver.find_element(By.NAME,"Plus").click()
-        driver.find_element(By.NAME,"Five").click()
-        driver.find_element(By.NAME,"Equals").click()
-        assert self.get_result() == expected_result
+    def get_plus(self):
+        return self.driver.find_element(By.NAME,"Plus").click()
 
-    def teardown_class(self):
-        driver.quit()
+    def get_five(self):
+        return self.driver.find_element(By.NAME,"Five").click()
+
+    def get_equals(self):
+        return self.driver.find_element(By.NAME,"Equals").click()
 
     def get_result(self):
-        return driver.find_element_by_xpath("//*[@AutomationId='CalculatorResults']").text.replace("Display is","").strip()
+        return self.driver.find_element(By.XPATH, "//*[@AutomationId='CalculatorResults']").\
+            text.replace("Display is","").strip()
+
+
